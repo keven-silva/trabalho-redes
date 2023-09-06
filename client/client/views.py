537555e78f5bcdb3bf64245d64c90ecee1904104
keven silva <keven.almeida.8179@gmail.com>
2023-09-06 16:59:29 -0300

    adicionando versão inicial

--- client/client/views.py
+++ client/client/views.py
@@ -1,9 +1,43 @@
+import socket
 from django.views import View
 from django.shortcuts import render
+
+
+class Client:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+
+    def send_msg(self, msg: str):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            if not msg:
+                s.sendall("Failed".encode())
+            else:
+                s.sendall(msg.encode())
+            
+            data = s.recv(1024)
+
+        print(f"Received {data!r}")
 
 
 class IndexView(View):
     template_name = "base.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        context = {"title": "Cliente"}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        host = request.POST.get("host")
+        port = request.POST.get("port")
+        msg = request.POST.get("msg")
+
+        if len(msg) == 0:
+            msg = None 
+
+        client = Client(host, int(port))
+        client.send_msg(msg)
+        
+        context = {"title": "Cliente"}
+        return render(request, self.template_name, context)
--- server/server/views.py
+++ server/server/views.py
@@ -1,5 +1,32 @@
+import socket
 from django.views import View
 from django.shortcuts import render
+from concurrent import futures
+
+
+class Server:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+
+    def configure(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            conn, addr = s.accept()
+
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if data == b"Failed":
+                        conn.sendall(b"Failed")
+                        return "","Failed"
+                    else:
+                        conn.sendall(data)
+                    
+                
+                    return data, "Sucess"
 
 
 class IndexView(View):
@@ -7,3 +34,17 @@ class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        host = request.POST.get("host")
+        port = request.POST.get("port")
+
+        server = Server(host, int(port))
+        future = futures.ThreadPoolExecutor().submit(server.configure)
+        result, status = future.result()
+        
+        if result:
+            result = result.decode()
+        
+        context = {"title": "Servidor", "connected": True, "message": result, "status": status}
+        return render(request, self.template_name, context)
